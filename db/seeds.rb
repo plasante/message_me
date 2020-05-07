9ebc84e55a983be5f8b9a35cb3914ecf911e7348
Pierre Lasante <pierre.lasante@videotron.ca>
2020-05-07 07:56:10 -0400

    Seed the db with User and Message

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.delete_all
-User.create(username: "plasante", password: "123456")
-User.create(username: "cspenard", password: "123456")
-User.create(username: "jspenard", password: "123456")
+plasante = User.create(username: "plasante", password: "123456")
+cspenard = User.create(username: "cspenard", password: "123456")
+jspenard = User.create(username: "jspenard", password: "123456")
 User.create(username: "miko", password: "123456")
 User.create(username: "micky", password: "123456")
 User.create(username: "charli", password: "123456")
+
+Message.delete_all
+Message.create(body: "Message from 'plasante'", user_id: "#{plasante.id}")
+Message.create(body: "Message from 'cspenard'", user_id: "#{cspenard.id}")
+Message.create(body: "Message from 'jspenard'", user_id: "#{jspenard.id}")
