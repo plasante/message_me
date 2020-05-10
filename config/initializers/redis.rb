6f4f75e2f3655ace47ae51c70c4e5f5e2eee9f51
Pierre Lasante <pierre.lasante@videotron.ca>
2020-05-10 08:40:40 -0400

    Redis redefined

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-$redis = Redis.new(url: ENV["REDIS_URL"])
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
